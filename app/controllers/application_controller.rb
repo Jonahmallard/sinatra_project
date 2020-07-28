84d6de7e44bd1b7db76d788bb4a1cbd31d4f87af
jonah <benjaminjmallard@gmail.com>
2020-07-28 02:25:34 -0500

    users '/signup' page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, SecureRandom.hex(64)
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
 class UsersController < ApplicationController
-
+  get '/signup' do
+    erb :'users/signup'
+  end
 end
